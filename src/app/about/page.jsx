1a3c4a45c6ea16f2e751ee777b89dcb7f2872ab7
srseen <seereen.2540@gmail.com>
2025-03-16 11:02:41 +0700

    style: center-align heading in About page and adjust layout in Home page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,9 @@
 export default function About() {
   return (
     <div className="flex flex-col items-center justify-center m-3">
-      <h3 className="text-3xl font-bold mb-4">Purpose of This Website</h3>
+      <h3 className="text-3xl font-bold text-center mb-4">
+        Purpose of This Website
+      </h3>
       <p className="text-lg text-center max-w-2xl">
         This website serves as a portfolio to showcase my projects and skills.
         It is designed to provide potential employers and collaborators with an
