6156ccbec5921579d7fc52cfc1f0b6a52f1e0d0c
srseen <seereen.2540@gmail.com>
2025-06-23 14:20:37 +0700

    refactor Skills component to simplify skill display and enhance organization

--- src/app/skills/page.jsx
+++ src/app/skills/page.jsx
@@ -1,66 +1,24 @@
 import SkillsData from "@/data/skills_data.jsx";
-
-function SkillSection({ title, skills }) {
-  return (
-    <div className="w-full p-4">
-      <h2 className="text-2xl font-bold mb-4">{title}</h2>
-      <ul>
-        {skills.map((skill) => (
-          <li key={skill.name} className="mb-4">
-            <h3 className="text-lg font-bold">{skill.name}</h3>
-            {Array.isArray(skill.description) ? (
-              skill.description.map((desc) => (
-                <p key={desc} className="block">
-                  - {desc}
-                </p>
-              ))
-            ) : (
-              <p className="block">- {skill.description}</p>
-            )}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+import { CardSkill } from "@/components/molecules/Card";
 
 export default function Skills() {
-  const frontendSkills = SkillsData.find(
-    (category) => category.category === "Frontend"
-  );
-  const backendSkills = SkillsData.find(
-    (category) => category.category === "Backend"
-  );
-  const databaseSkills = SkillsData.find(
-    (category) => category.category === "Database"
-  );
-  const devOpsSkills = SkillsData.find(
-    (category) => category.category === "DevOps"
-  );
-
-  const testingSkills = SkillsData.find(
-    (category) => category.category === "Testing"
-  );
-
   return (
     <div className="flex flex-col items-center justify-center mx-10">
-      <h1 className="text-4xl font-bold mb-6 text-center mt-4">Skills</h1>
-      <div className="flex flex-wrap justify-center ">
-        <div className="w-full md:w-1/3 lg:w-1/5">
-          <SkillSection title="Frontend" skills={frontendSkills.skills} />
-        </div>
-        <div className="w-full md:w-1/3 lg:w-1/5">
-          <SkillSection title="Backend" skills={backendSkills.skills} />
-        </div>
-        <div className="w-full md:w-1/3 lg:w-1/5">
-          <SkillSection title="Database" skills={databaseSkills.skills} />
-        </div>
-        <div className="w-full md:w-1/3 lg:w-1/5">
-          <SkillSection title="DevOps" skills={devOpsSkills.skills} />
-        </div>
-        <div className="w-full md:w-1/3 lg:w-1/5">
-          <SkillSection title="Testing" skills={testingSkills.skills} />
-        </div>
+      <h1 className="text-4xl font-bold mb-6 text-center mt-4">My Skills</h1>
+      <p className="text-lg text-center mb-6">
+        I am a Junior Fullstack Programmer with a passion for learning and
+        growing in the field of software development. Here are some of the
+        skills I have acquired:
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
+        {SkillsData.map((category) => (
+          <div key={category.category} className="w-full">
+            <CardSkill
+              name={category.category}
+              skill={category.skills.map((s) => s.name)}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
