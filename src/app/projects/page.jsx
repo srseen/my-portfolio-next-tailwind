e1a3d42aaefa467962a037653b4d85a5891e5bc9
srseen <seereen.2540@gmail.com>
2025-06-23 14:40:44 +0700

    refactor Project component: adjust heading and paragraph styling for improved layout

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -4,8 +4,8 @@ import ProjectData from "@/data/project_data";
 export default function Project() {
   return (
     <div className="max-w-4xl mx-auto p-6">
-      <h1 className="text-4xl font-bold text-center  mb-6">My Projects</h1>
-      <p className="text-lg  text-center mb-6">
+      <h1 className="text-4xl font-bold text-center mb-6 mt-4">My Projects</h1>
+      <p className="text-lg text-center mb-6">
         Hi, I&apos;m a passionate programmer with experience in various
         technologies. Explore my projects and get to know more about my work.
       </p>
