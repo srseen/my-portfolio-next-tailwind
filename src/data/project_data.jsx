1d41a2291aa1d7027373707b0c4b7bf2af7e5720
srseen <seereen.2540@gmail.com>
2025-06-23 23:13:34 +0700

    Update CardProject component and project data for new demo website images and logos

--- src/data/project_data.jsx
+++ src/data/project_data.jsx
@@ -1,6 +1,6 @@
 const ProjectData = [
   {
-    image: "/public/Image/my_website.png",
+    image: "/public/Image-website/my_website.png",
     url: "https://seereen.website",
     title: "Portfolio Website",
     description:
@@ -18,34 +18,70 @@ const ProjectData = [
     ],
   },
   {
-    image: "/public/Image/homepage-seen-computer-screen.jpg",
+    image: "/public/Image-website/demo-website.jpg",
+    url: "https://demo-website.com",
     title: "Project 3",
     description: "Description of project 3.",
+    logo: [
+      "/public/svg-tech-stack/react.svg",
+      "/public/svg-tech-stack/tailwindcss.svg",
+      "/public/svg-tech-stack/nextjs.svg",
+    ],
   },
   {
-    image: "/public/Image/homepage-seen-computer-screen.jpg",
+    image: "/public/Image-website/demo-website.jpg",
+    url: "https://demo-website.com",
     title: "Project 4",
     description: "Description of project 4.",
+    logo: [
+      "/public/svg-tech-stack/react.svg",
+      "/public/svg-tech-stack/tailwindcss.svg",
+      "/public/svg-tech-stack/nextjs.svg",
+    ],
   },
   {
-    image: "/public/Image/homepage-seen-computer-screen.jpg",
+    image: "/public/Image-website/demo-website.jpg",
+    url: "https://demo-website.com",
     title: "Project 5",
     description: "Description of project 5.",
+    logo: [
+      "/public/svg-tech-stack/react.svg",
+      "/public/svg-tech-stack/tailwindcss.svg",
+      "/public/svg-tech-stack/nextjs.svg",
+    ],
   },
   {
-    image: "/public/Image/homepage-seen-computer-screen.jpg",
+    image: "/public/Image-website/demo-website.jpg",
+    url: "https://demo-website.com",
     title: "Project 6",
     description: "Description of project 6.",
+    logo: [
+      "/public/svg-tech-stack/react.svg",
+      "/public/svg-tech-stack/tailwindcss.svg",
+      "/public/svg-tech-stack/nextjs.svg",
+    ],
   },
   {
-    image: "/public/Image/homepage-seen-computer-screen.jpg",
+    image: "/public/Image-website/demo-website.jpg",
+    url: "https://demo-website.com",
     title: "Project 7",
     description: "Description of project 7.",
+    logo: [
+      "/public/svg-tech-stack/react.svg",
+      "/public/svg-tech-stack/tailwindcss.svg",
+      "/public/svg-tech-stack/nextjs.svg",
+    ],
   },
   {
-    image: "/public/Image/homepage-seen-computer-screen.jpg",
+    image: "/public/Image-website/demo-website.jpg",
+    url: "https://demo-website.com",
     title: "Project 8",
     description: "Description of project 8.",
+    logo: [
+      "/public/svg-tech-stack/react.svg",
+      "/public/svg-tech-stack/tailwindcss.svg",
+      "/public/svg-tech-stack/nextjs.svg",
+    ],
   },
 ];
 export default ProjectData;
