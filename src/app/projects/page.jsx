5e7f3fa1d7254f2a8f900e2967165d08c6d00d3e
srseen <seereen.2540@gmail.com>
2025-06-23 14:20:43 +0700

    add ProjectData and refactor Project component to utilize CardProject for dynamic project display

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,3 +1,6 @@
+import { CardProject } from "@/components/molecules/Card";
+import ProjectData from "@/data/project_data";
+
 export default function Project() {
   return (
     <div className="max-w-4xl mx-auto p-6">
@@ -7,22 +10,13 @@ export default function Project() {
         technologies. Explore my projects and get to know more about my work.
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="primary-color p-4 rounded-lg shadow-md">
-          <h2 className="text-2xl font-semibold  mb-2">Project 1</h2>
-          <p>Description of project 1.</p>
-        </div>
-        <div className="primary-color p-4 rounded-lg shadow-md">
-          <h2 className="text-2xl font-semibold 0 mb-2">Project 2</h2>
-          <p>Description of project 2.</p>
-        </div>
-        <div className="primary-color p-4 rounded-lg shadow-md">
-          <h2 className="text-2xl font-semibold mb-2">Project 3</h2>
-          <p>Description of project 3.</p>
-        </div>
-        <div className="primary-color p-4 rounded-lg shadow-md">
-          <h2 className="text-2xl font-semibold mb-2">Project 4</h2>
-          <p>Description of project 4.</p>
-        </div>
+        {ProjectData.map((project, index) => (
+          <CardProject
+            key={index}
+            title={project.title}
+            description={project.description}
+          />
+        ))}
       </div>
     </div>
   );
