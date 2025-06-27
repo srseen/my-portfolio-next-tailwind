fdfd8d76feec0cbaa6ed984b0d172805d4f902e6
srseen <seereen.2540@gmail.com>
2025-06-28 02:40:33 +0700

    Refactor CardProject component for improved layout and consistency

--- src/components/molecules/CardProject.jsx
+++ src/components/molecules/CardProject.jsx
@@ -6,29 +6,30 @@ export function CardProject({ image, url, title, description, logo }) {
       {...(url
         ? { href: url, target: "_blank", rel: "noopener noreferrer" }
         : {})}
-      className="primary-color w-full h-full p-4 rounded-lg shadow-md flex flex-col transition-transform duration-300 hover:scale-105 max-w-sm mx-auto justify-between"
+      className="primary-color w-80 h-96 p-4 rounded-lg shadow-md flex flex-col justify-between items-center transition-transform duration-300 hover:scale-105 max-w-sm mx-auto overflow-hidden"
     >
       <img
         src={image}
         alt={title}
-        className="w-full h-40 object-cover rounded-lg my-4 shadow-lg"
+        className="object-cover rounded-lg my-4 shadow-lg w-full h-40"
       />
-
-      <div className="text-white flex flex-col w-full flex-1 justify-between items-center">
-        <h2 className="text-lg font-semibold mb-2 line-clamp-2 text-black dark:text-white text-center">
-          {title}
+      <div className="text-white flex flex-col justify-between items-center flex-grow w-full">
+        <h2 className="text-lg font-semibold text-black dark:text-white text-center mb-2 flex-grow-0">
+          {title.length > 30 ? title.substring(0, 30) + "..." : title}
         </h2>
-        <p className="text-sm line-clamp-3 mb-4 text-black dark:text-white text-center w-full">
-          {description}
+        <p className="text-sm text-black dark:text-white text-center flex-grow mb-4 overflow-hidden text-ellipsis flex-grow">
+          {description.length > 100
+            ? description.substring(0, 100) + "..."
+            : description}
         </p>
         {logo && (
-          <div className="flex flex-wrap justify-center items-center gap-2 mt-auto secondary-color p-2 rounded-full self-center">
+          <div className="flex flex-wrap justify-center items-center gap-2 secondary-color p-2 rounded-full mt-auto self-center flex-grow-0">
             {logo.map((logoItem, index) => (
               <img
                 key={index}
                 src={logoItem}
                 alt="Tech Logo"
-                className="w-8 h-8"
+                className="w-8 h-8 object-contain"
               />
             ))}
           </div>
